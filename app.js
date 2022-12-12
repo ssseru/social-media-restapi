db7e3a359965dad7bc768b705e2ea0dcbdd135cf
Sai Seru <saiseru@publicisgroupe.net>
2022-12-12 15:46:49 +0530

    resolving cors issue

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 // for application/json
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
