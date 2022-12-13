ad44eede2ea99b8be57dec8b72d4d7c062507610
Sai Seru <saiseru@publicisgroupe.net>
2022-12-13 10:41:00 +0530

    added validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +18,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      message: "Validation Failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
+  }
   const { title, content } = req.body;
   res.status(201).json({
     message: "Post created",
