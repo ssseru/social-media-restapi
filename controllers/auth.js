a9953ea1f6021832222e72f77246595b53d4e4e2
Sai Seru <saiseru@publicisgroupe.net>
2022-12-13 16:56:46 +0530

    starting with login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,3 +35,33 @@ exports.signup = (req, res, next) => {
       next(err);
     });
 };
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User.findOne({
+    email: email,
+  })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No user found");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong Password");
+        error.statusCode = 401;
+        throw error;
+      }
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
