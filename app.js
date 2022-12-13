11f039539248db21a8b4000117b4aad5d9acdeeb
Sai Seru <saiseru@publicisgroupe.net>
2022-12-13 15:49:59 +0530

    user signup validation

--- app.js
+++ app.js
@@ -61,8 +61,10 @@ app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
+  const data = error.data;
   res.status(status).json({
     message: message,
+    data: data,
   });
 });
 
