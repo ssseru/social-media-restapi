d8182fff8ddc6cbf3e6bd1b2ac7120f1451f5b0e
Sai Seru <saiseru@publicisgroupe.net>
2022-12-13 15:26:29 +0530

    adding user model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -54,6 +55,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
