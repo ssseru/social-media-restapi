d75e4bfa7986f0ea9932d808607181b7bd8b1dcc
Sai Seru <saiseru@publicisgroupe.net>
2022-12-14 11:55:04 +0530

    status functionality added

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,3 +78,49 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getUserStatus = (req, res, next) => {
+  console.log("getStatus", req.userId);
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("Could not find user");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        status: user.status,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.updateUserStatus = (req, res, next) => {
+  const newStatus = req.body.status;
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("Could not find user");
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: "statusUpdated",
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const { body } = require("express-validator");
 const User = require("../models/user");
 const authController = require("../controllers/auth");
 
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.put(
@@ -27,5 +29,14 @@ router.put(
 );
 
 router.post("/login", authController.login);
+
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.put(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
