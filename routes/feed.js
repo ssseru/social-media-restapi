13fd1b84094656b32695122bdb28457fe60a5157
Sai Seru <saiseru@publicisgroupe.net>
2022-12-14 10:52:58 +0530

    isAuth to all routes

--- routes/feed.js
+++ routes/feed.js
@@ -10,6 +10,7 @@ router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
   "/post",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -21,6 +22,7 @@ router.get("/post/:postId", feedController.getPost);
 
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -28,6 +30,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
