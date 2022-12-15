e827a670bb7bc72735ede53c5c9e3ab590b47249
Sai Seru <saiseru@publicisgroupe.net>
2022-12-15 12:30:43 +0530

    handling data

--- app.js
+++ app.js
@@ -65,6 +65,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An error occurred";
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
