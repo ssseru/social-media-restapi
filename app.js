88e911096da0f19b9945783fe95feaee912f9fa4
Sai Seru <saiseru@publicisgroupe.net>
2022-12-15 15:44:52 +0530

    using auth token in graphql

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { v4: uuidv4 } = require("uuid");
 const { graphqlHTTP } = require("express-graphql");
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
 // const feedRoutes = require("./routes/feed");
 // const authRoutes = require("./routes/auth");
 
@@ -61,6 +62,8 @@ app.use((req, res, next) => {
 
 // app.use("/feed", feedRoutes);
 // app.use("/auth", authRoutes);
+
+app.use(auth);
 
 app.use(
   "/graphql",
