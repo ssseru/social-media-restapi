4cc0f7e3d366b8153e91c5fa49b066a4eaa0461b
Sai Seru <saiseru@publicisgroupe.net>
2022-12-16 15:47:44 +0530

    solved bug comming in filePath in clearImage

--- util/file.js
+++ util/file.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 const clearImage = (filePath) => {
-  filePath = path.join(__dirname, ".", filePath);
+  filePath = path.join(__dirname, "..", filePath);
   console.log("inside clear image", filePath);
   fs.unlink(filePath, (err) => console.log(err));
 };
