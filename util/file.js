ee83754b696f745317af2bd7962c0a3a3c2d2211
Sai Seru <saiseru@publicisgroupe.net>
2022-12-16 15:43:25 +0530

    deleting posts

---
+++ util/file.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const fs = require("fs");
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, ".", filePath);
+  console.log("inside clear image", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
+
+exports.clearImage = clearImage;
